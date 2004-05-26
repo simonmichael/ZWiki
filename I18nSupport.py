69c35f16af9d47b05a02ece64a740fac51ce75d4
simon <simon@joyful.com>
2004-05-26 18:13:18 +0000

    prevent dtml-translate tag errors when PTS is not installed

--- I18nSupport.py
+++ I18nSupport.py
@@ -28,16 +28,12 @@ try:
                     REQUEST.RESPONSE.setHeader( 
                         'Content-Type','text/html; charset=utf-8')
                 return MessageIDFactory.__call__(self,ustr,default)
-        _ = MessageIDFactoryWithUtf8Fix(
-            'zwiki',
-            # trying to make PTS return utf-8 translations so testI18n.py
-            # can be utf8-encoded.. no effect
-            default_encoding='utf-8')
+        _ = MessageIDFactoryWithUtf8Fix('zwiki')
         from Products.PageTemplates.PageTemplateFile import PageTemplateFile
         from Globals import HTMLFile, DTMLFile
         BLATHER('using PlacelessTranslationService for i18n')
 
-    elif USE_LOCALIZER:
+    elif USE_LOCALIZER: # not supported at the moment
         from Products.Localizer import Gettext
         _ = Gettext.translation(globals())
         from Products.Localizer import LocalPageTemplateFile as PageTemplateFile
@@ -50,5 +46,27 @@ except (ImportError, NameError):
     def _(s): return s
     from Products.PageTemplates.PageTemplateFile import PageTemplateFile
     from Globals import HTMLFile, DTMLFile
+    class DummyTranslateTag:
+        name='translate'
+        blockContinuations=()
+        _msgid = None
+        _domain = None
+        def __init__(self, blocks): 
+            self.blocks=blocks
+            tname, args, section = blocks[0]
+            self.__name__="%s %s" % (tname, args)
+        def render(self, md):
+            r=[]
+            for tname, args, section in self.blocks:
+                __traceback_info__=tname
+                r.append(section(None, md))
+            if r:
+                if len(r) > 1: r = "(%s)\n" % join(r,' ')
+                else: r = r[0]
+            else:
+                r = ''
+            return r + '\n'
+        __call__=render
+    from DocumentTemplate.DT_String import String
+    String.commands['translate'] = DummyTranslateTag
     BLATHER('using no i18n')
-
