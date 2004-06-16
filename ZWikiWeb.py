6b5effa558e95465ab56518559f21ab7f6086fb5
bob+zwiki <bob+zwiki@mcelrath.org>
2004-06-16 04:34:12 +0000

    ignore vim swapfiles on import

--- ZWikiWeb.py
+++ ZWikiWeb.py
@@ -94,7 +94,7 @@ def addZWikiWebFromFs(self, new_id, title='', wiki_type='zwikidotorg',
     filenames = os.listdir(dir)
     # hmm auto-cataloging is really slowing this down!
     for filename in filenames:
-        if re.match(r'(?:\.(?:svn|cvs)|CVS)', filename): continue
+        if re.match(r'(?:\.(?:svn|cvs|.*\.swp$)|CVS)', filename): continue
         m = re.search(r'(.+)\.(.+)',filename)
         id, type = filename, ''
         if m: id, type = m.groups()
