c8e8315977cfddb2e38266dd253b1bd3a4204778
simon <simon@joyful.com>
2004-08-11 00:18:05 +0000

    plugins: docs

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,15 +1,17 @@
 # experimental package for non-core plugin modules
 #
 # A Zwiki plugin is a module providing a mixin class that extends
-# ZWikiPage, thereby adding extra features to all wiki pages.  Like page
-# types, it should be possible for zwiki plugins to be provided by other
-# products also.
-#
+# ZWikiPage, thereby adding extra features to all wiki pages at startup.
+# A true plugin (a) can be removed without ill effect aside from disabling
+# the feature it provides, and (b) could be provided by a separate
+# product.
+# 
 # Mixins which used to be in the main ZWiki package are gradually being
-# moved here. Some of them may still have hard-coded dependencies in other
-# parts of the code. Code has been completely plugin-ized if: you can
-# remove it without ill effect other than disabling the feature and if it
-# could be provided by another product.
+# moved here and pluginised (some of them still have hard-coded
+# dependencies in other parts of the code).
+#
+# page types are another kind of add-on, residing in their own pagetypes
+# package. We may invent others.
 
 #def registerPlugin(p): pass
 
