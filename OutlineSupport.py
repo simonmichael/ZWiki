43d2c13093ee874a8590498d930aaeeedcde6c96
simon <simon@joyful.com>
2004-08-27 15:30:50 +0000

    fix for expandable bullet list patch ?

--- OutlineSupport.py
+++ OutlineSupport.py
@@ -751,10 +751,10 @@ class OutlineRenderingMixin:
                             got.append('%s <li>[%s]</li>' % (indent,i))
                     if not (n[0]==here and suppress_current): #XXX temp
                         got.append("</ul>")
-                    if not (n[0]==here and suppress_current):
-                        got.append('%s </li>' % indent)
                 else:
                     got[-1] += ' ...' # a parent whose children were omitted
+                if not (n[0]==here and suppress_current):
+                    got.append('%s </li>' % indent)
             else:
                 got.append('%s <li>[%s]</li>' % (indent,n))
         if recursing: return got
