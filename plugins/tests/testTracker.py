eb97b9aa16abdc527c2fa8d99bd2602c6a4c3eb8
simon <simon@joyful.com>
2004-10-01 23:04:59 +0000

    short_issue_names property enables short # prefix for issue page names

--- plugins/tests/testTracker.py
+++ plugins/tests/testTracker.py
@@ -8,28 +8,9 @@ ZopeTestCase.installProduct('ZWiki')
 ZopeTestCase.installProduct('ZCatalog')
 
 
-class Tests(ZopeTestCase.ZopeTestCase):
+class TrackerSetupTests(ZopeTestCase.ZopeTestCase):
     def afterSetUp(self):
         zwikiAfterSetUp(self)
-
-    def test_isIssue(self):
-        def isIssue(name):
-            self.p.create(page=name)
-            return self.p.pageWithName(name).isIssue()
-        self.failIf(isIssue('blah'))
-        self.failIf(isIssue('IssueNo'))
-        self.assert_(isIssue('IssueNo1'))
-        self.assert_(isIssue('IssueNo1 blah'))
-        self.assert_(isIssue('#1 blah'))
-        self.failIf(isIssue('1'))
-
-    def test_upgradeIssueProperties(self):
-        self.p.create('IssueNo0001')
-        p = self.p.pageWithName('IssueNo0001')
-        self.assert_(not hasattr(p,'severity'))
-        p.upgradeIssueProperties()
-        self.assert_(hasattr(p,'severity'))
-        self.assertEqual(p.severity,'normal')
 
     def test_setupTracker(self):
         p = self.p
@@ -54,11 +35,55 @@ class Tests(ZopeTestCase.ZopeTestCase):
         f.IssueNo0001FirstIssue.createNextIssue('test')
         self.assertEqual(f.IssueNo0002Test.parents,['IssueTracker'])
 
+    def test_upgradeIssueProperties(self):
+        self.p.create('IssueNo0001')
+        p = self.p.pageWithName('IssueNo0001')
+        self.assert_(not hasattr(p,'severity'))
+        p.upgradeIssueProperties()
+        self.assert_(hasattr(p,'severity'))
+        self.assertEqual(p.severity,'normal')
+
+
+class TrackerTests(ZopeTestCase.ZopeTestCase):
+    def afterSetUp(self):
+        zwikiAfterSetUp(self)
+        self.p.setupTracker()
+
+    def test_isIssue(self):
+        isIssue = self.p.isIssue
+        self.assert_(not isIssue('blah'))
+        self.assert_(not isIssue('1'))
+        self.assert_(not isIssue('IssueNo'))
+        self.assert_(    isIssue('IssueNo1'))
+        self.assert_(not isIssue('#1'))
+        self.p.folder().short_issue_names = 1
+        self.assert_(    isIssue('IssueNo1'))
+        self.assert_(    isIssue('#1'))
+        
+    def test_issueNumberFrom(self):
+        issueNumberFrom = self.p.issueNumberFrom
+        self.failIf(issueNumberFrom('#1 blah'))
+        self.assertEqual(issueNumberFrom('IssueNo1'),1)
+        self.assertEqual(issueNumberFrom('IssueNo1 blah'),1)
+        self.p.folder().short_issue_names = 1
+        self.assertEqual(issueNumberFrom('IssueNo1'),1)
+        self.assertEqual(issueNumberFrom('#2 blah'),2)
+
+    def test_createNextIssue(self):
+        self.p.createNextIssue('b')
+        self.assert_(self.p.pageWithName('IssueNo0002 b'))
+        self.p.folder().short_issue_names = 1
+        self.p.createNextIssue('c')
+        self.assert_(self.p.pageWithName('#3 c'))
+        self.assertEqual(self.p.issueCount(),3)
+        
+
 if __name__ == '__main__':
     framework(descriptions=1, verbosity=2)
 else:
     import unittest
     def test_suite():
         suite = unittest.TestSuite()
-        suite.addTest(unittest.makeSuite(Tests))
+        suite.addTest(unittest.makeSuite(TrackerSetupTests))
+        suite.addTest(unittest.makeSuite(TrackerTests))
         return suite
