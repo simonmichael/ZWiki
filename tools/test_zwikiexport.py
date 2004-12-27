d25c9c3e35039b1faa5279120ab7819e7bce17f9
simon <simon@joyful.com>
2004-12-27 03:23:14 +0000

    add tools folder and a working import-files-to-wiki utility

---
+++ tools/test_zwikiexport.py
@@ -0,0 +1,8 @@
+import unittest
+
+class ZwikiExportTests(unittest.TestCase):
+    def testSomething(self):
+        self.assertEquals(0 + 1, 1)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tools/zwikiexport.py
@@ -0,0 +1,23 @@
+""" 
+zwikiexport.py - export a set of zwiki pages as static files.
+
+(c) 2004 SKWM, GNU GPL.
+
+Usage:
+
+   cd directorytoreceivefiles
+   zopectl run .../Products/ZWiki/tools/zwikiexport.py [options] /path/to/wikifolderorpage
+
+Options:
+
+   -h, --help
+
+   -n, --dry-run
+
+Notes/stories:
+export to var directory and/or as a downloadable tarball
+run from web or commandline or both ?
+export whole wiki
+export only current page and offspring
+page type is reflected in file suffix
+allow round trip with zwikiimport
