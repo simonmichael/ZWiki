91747547a7800f2181e9bb2605f99e5aebb85c21
simon <simon@joyful.com>
2006-02-12 02:21:31 +0000

    fix pagetypes tests

--- pagetypes/pagetypes_tests.py
+++ pagetypes/pagetypes_tests.py
@@ -11,7 +11,7 @@ class PageTypesTests(ZopeTestCase.ZopeTestCase):
     def test_ZwikiStxPageType(self):
         self.p.edit(text='! PageOne PageTwo\n',type='stx')
         self.assertEquals(self.p.render(bare=1),
-                          '<p> PageOne PageTwo</p>\n<p></p>\n')
+                          '<p> PageOne PageTwo</p>\n<p>\n</p>\n')
 
     #def test_ZwikiRstPageType(self):
     #    self.p.edit(text='! PageOne PageTwo\n',type='msgrstprelinkdtmlfitissuehtml')
@@ -21,15 +21,14 @@ class PageTypesTests(ZopeTestCase.ZopeTestCase):
 
     def test_ZwikiHtmlPageType(self):
         self.p.edit(text='! PageOne PageTwo\n',type='html')
-        self.assertEquals(self.p.render(bare=1),' PageOne PageTwo\n\n')
+        self.assertEquals(self.p.render(bare=1),' PageOne PageTwo\n\n\n')
         self.p.edit(text='PageOne\n',type='html')
         self.assertEquals(
             self.p.render(bare=1),
-            'PageOne<a class="new" href="http://nohost/test_folder_1_/wiki/TestPage/createform?page=PageOne" title="create this page">?</a>\n\n')
+            'PageOne<a class="new visualNoPrint" href="http://nohost/test_folder_1_/wiki/TestPage/createform?page=PageOne" title="create this page">?</a>\n\n\n')
 
     def test_ZwikiWwmlPageType(self):
-        self.p.edit(text = \
-"""
+        self.p.edit(text = """
 This is the first paragraph.  There should be a paragraph marker before it,
 and it should wrap nicely around.
 
@@ -74,15 +73,56 @@ Let's see if AutomaticURLLinking works yet:
 
 And [these words] should be linked, too.
 """)
-        self.assertEquals(self.p.render(bare=1),
-"""
+        self.assertEquals(
+            self.p.render(bare=1),
+            """\
+<p>This is the first paragraph.  There should be a paragraph marker before it,
+and it should wrap nicely around.</p>
+<p>---------
+That should have produced a horizontal rule.</p>
+<p>The last word should be ''emphasized''.</p>
+<p>The last word should be '''strong'''</p>
+<p>Can we do both ''emph'' and '''strong''' on the same line?</p>
+<h2>How about two times:  ''emph1'' followed by ''emph2''?</h2>
+
+<ul>
+<li>one bullet, with a WikiName<a class="new visualNoPrint" href="http://nohost/test_folder_1_/wiki/TestPage/createform?page=WikiName" title="create this page">?</a>
+        * second bullet, with an UnknownWikiName<a class="new visualNoPrint" href="http://nohost/test_folder_1_/wiki/TestPage/createform?page=UnknownWikiName" title="create this page">?</a>
+                1. nested digit
+                2. nested digit</li>
+
+</ul>
+
+<ol>
+<li> Top-level numbered list
+        2. Top-level numbered list
+                <em> nested bullet
+                </em> nested bullet
+        3. See if we keep the numbering here!</li>
+
+</ol>
+<p>        Term: a definition
+        ''Marked-up Term'': another definition, but with ''markup''
+        : a definition without a term</p>
+<p>  This should be monospaced,
+    and indented manually</p>
+<h2>and now this is another normal paragraph.</h2>
+<p>  Here is some more pre-formatted text.
+        * followed by a bullet</p>
+<p>Let's see if AutomaticURLLinking<a class="new visualNoPrint" href="http://nohost/test_folder_1_/wiki/TestPage/createform?page=AutomaticURLLinking" title="create this page">?</a> works yet:
+        <em> <a href="http://www.palladion.com">http://www.palladion.com</a>
+        </em> <a href="ftp://www.neosoft.com/pub/users/t/tseaver">ftp://www.neosoft.com/pub/users/t/tseaver</a>
+        * <a href="mailto:tseaver@palladion.com">mailto:tseaver@palladion.com</a></p>
+<p>And [these words]<a class="new visualNoPrint" href="http://nohost/test_folder_1_/wiki/TestPage/createform?page=these%20words" title="create this page">?</a> should be linked, too.</p>
+<p>
+</p>
 """)
 
     def test_ZwikiPlaintextPageType(self):
         self.p.folder().allowed_page_types = ['plaintext']
         self.p.edit(text='! PageOne PageTwo\n',type='plaintext')
         self.assertEquals(self.p.render(bare=1),
-                          '<pre>\n! PageOne PageTwo\n\n</pre>\n\n')
+                          '<pre>\n! PageOne PageTwo\n\n</pre>\n\n\n')
         del self.p.folder().allowed_page_types
 
 
