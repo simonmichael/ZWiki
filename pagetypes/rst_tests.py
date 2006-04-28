0987512ed54aa0bb35b103337924e2648ac086f7
simon <simon@joyful.com>
2006-04-28 18:52:49 +0000

    update stefan's rst-dtml patch, add test

--- pagetypes/rst_tests.py
+++ pagetypes/rst_tests.py
@@ -14,3 +14,22 @@ class Tests(ZwikiTestCase):
         self.assertEquals(
             self.p.render(bare=1),
             '<blockquote>\nPageOne PageTwo</blockquote>\n<p>\n</p>\n')
+
+    def test_dtml_in_rst(self):
+        p = self.p
+        
+        # disabled by default ?
+        p.edit(text='<dtml-var "1+1">',type='rst')
+        self.assert_(p.hasDynamicContent())
+        self.assert_(not p.dtmlAllowed())
+        self.assertEquals(
+            p.render(bare=1),
+            '<p>&lt;dtml-var &quot;1+1&quot;&gt;\n\n</p>\n')
+
+        # can be enabled ?
+        p.allow_dtml = 1
+        self.assert_(p.dtmlAllowed())
+        # XXX need both of these right here, why ?
+        p.clearCache(); p.cook()
+        self.assertEquals(p.render(bare=1), '2')
+
