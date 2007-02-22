35635c30c800fc33bd72b96dc5d56c024e61e2c5
betabug.darcs <betabug.darcs@betabug.ch>
2007-02-22 13:48:29 +0000

    format comment header for HTML pages #1085

--- pagetypes/html.py
+++ pagetypes/html.py
@@ -26,6 +26,32 @@ class PageTypeHtml(PageTypeBaseHtml):
         t = page.addSkinTo(t,**kw)
         return t
 
+    def makeCommentHeading(self, page,
+                           subject, username, time, 
+                           message_id=None,in_reply_to=None):
+        """
+        Generate restructured text markup for a comment heading in a HTML page.
+
+        Note that we just work on the comment heading here. The content of the 
+        comment is left as is, not certain what to do with it. Users likely 
+        expect to be able to write comments like on every other page type
+        (e.g. with two newlines to format paragraphs) - but what kind of markup
+        would be expected on a html page? XXX
+        """
+        heading = '\n\n<p class="commentheading"> '
+        heading += '<strong>%s</strong> --' % (subject or '...')
+        if username: heading = heading + '%s, ' % (username)
+        heading += time or ''
+        heading += ' <a class="reference" href="%s?subject=%s%s#bottom">%s</a>' % (
+            page.pageUrl(),
+            quote(subject or ''),
+            ((message_id and '&amp;in_reply_to='+quote(message_id))
+             or ''),
+            _("reply"),
+            )
+        heading += '\n\n</p>'
+        return heading
+
 registerPageType(PageTypeHtml)
 
 # backwards compatibility - need this here for old zodb objects
