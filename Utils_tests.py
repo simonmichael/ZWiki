0881d1aafdfc042709945fd8905d9c9a3b7af9c5
Simon Michael <simon@joyful.com>
2008-04-25 21:40:02 +0000

    resolve testExcerptAt conflict

--- Utils_tests.py
+++ Utils_tests.py
@@ -24,3 +24,32 @@ class Tests(ZwikiTestCase):
         self.failIf(safe_hasattr(p,'muppets'))
         setattr(p, 'muppets', 'gonzo')
         self.assert_(safe_hasattr(p,'muppets'))
+
+    def test_excerptAt(self):
+        self.page.edit(text='This is a test of the<br />\n excerptAt method,')
+        self.assertEquals(self.page.excerptAt('excerptat',size=10,highlight=0),
+                          '\n excerptAt ')
+        #self.assertEquals(self.page.excerptAt('this*',size=21,highlight=1),
+        #                  '<span class="hit">This</span> is a tes')
+        #self.assertEquals(self.page.excerptAt('br',size=4),
+        #                  'e&lt;<span class="hit">br</span>&gt;\n')
+        # XXX temp
+        self.assertEquals(self.page.excerptAt('this*',size=21,highlight=1),
+                          '<span class="hit" style="background-color:yellow;font-weight:bold;">This</span> is a tes')
+        self.assertEquals(self.page.excerptAt('br',size=4),
+                          'e&lt;<span class="hit" style="background-color:yellow;font-weight:bold;">br</span> /')
+        self.assertEquals(self.page.excerptAt('<br />',size=4,highlight=0),
+                          '&lt;br /&gt;')
+        self.assertEquals(self.page.excerptAt('nomatch'), 'This is a test of the&lt;br /&gt;\n excerptAt method,')
+        self.assertEquals(self.page.excerptAt(''), 'This is a test of the&lt;br /&gt;\n excerptAt method,')
+
+    def test_summary(self):
+        p = self.page
+        p.edit(text='É')
+        self.assertEqual('É',p.summary())
+
+    def test_BLATHER(self):
+        BLATHER('E')                    # ascii
+        BLATHER('É')                    # utf-8
+        BLATHER(u'\xc9')                # unicode
+        
