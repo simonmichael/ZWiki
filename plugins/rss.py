e0d62a448fbb80ac65cc17685e539d3fde014de1
Simon Michael <simon@joyful.com>
2009-02-04 19:02:06 +0000

    tidy

--- plugins/rss.py
+++ plugins/rss.py
@@ -9,8 +9,8 @@ from Globals import InitializeClass
 
 from Products.ZWiki import Permissions
 from Products.ZWiki.Utils import BLATHER, html_quote
-from Products.ZWiki.plugins import registerPlugin
 from Products.ZWiki.I18n import _
+from Products.ZWiki.plugins import registerPlugin
 
 class PageRSSSupport:
     """
