ddb24f83ca277c0cbad80bc00cf6b9d313bd4982
Simon Michael <simon@joyful.com>
2009-02-05 13:02:10 +0000

    docs cleanups

--- conf.py
+++ conf.py
@@ -40,9 +40,9 @@ copyright = '1999-2009, Simon Michael & the Zwiki team'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '2'
+version = '2.0a1'
 # The full version, including alpha/beta/rc tags.
-release = '2pre'
+release = '2.0a1'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
