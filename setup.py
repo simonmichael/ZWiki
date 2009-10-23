be1c6ea57574f0c99b539c9ee5721e0153b9c120
Simon Michael <simon@joyful.com>
2009-10-23 23:52:49 +0000

    add setup script to prepare for easy pypi installation

---
+++ setup.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "Zwiki",
+    version = "2.0b1",
+    author = "Simon Michael",
+    author_email = "simon@joyful.com",
+    description = "Zwiki, the Zope-based wiki engine.",
+    long_description = "Zwiki, the Zope-based wiki engine.",
+    url = "http://zwiki.org",
+    platforms = [],
+    keywords = "wiki zope",
+    classifiers = [],
+    license = "GPL",
+    scripts = [],
+    py_modules = [
+      "__init__",
+      "Admin",
+      "CMF",
+      "Catalog",
+      "Comments",
+      "Defaults",
+      "Diff",
+      "Editing",
+      "History",
+      "Mail",
+      "Outline",
+      "OutlineSupport",
+      "PageTypes",
+      "Permissions",
+      "Regexps",
+      "Splitter",
+      "TextFormatter",
+      "Utils",
+      "Views",
+      "ZWikiPage",
+      ],
+    packages = find_packages(),
+    package_data = {
+      '':['*.txt'],
+      },
+    install_requires = [
+      'Zope2>=2.12',
+      ],
+    )
