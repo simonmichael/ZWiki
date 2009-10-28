855c49019e724cbcff213e03eabdd9c45cd1e4e9
Simon Michael <simon@joyful.com>
2009-10-28 14:18:41 +0000

    refactor rss support

--- plugins/rss.py
+++ plugins/rss.py
@@ -24,63 +24,14 @@ class PageRSSSupport:
 
     security.declareProtected(Permissions.View, 'pages_rss')
     def pages_rss(self, num=10, REQUEST=None):
-        """
-        Provide an RSS feed showing this wiki's recently created pages.
-        """
-        pages = self.pages(sort_on='creation_time',
-                            sort_order='reverse',
-                            sort_limit=num,
-                            isBoring=0)
-        if len(pages) > 0:
-            last_mod = pages[0].getObject().creationTime()
-        else:
-            last_mod = DateTime()
-        if self.handle_modified_headers(last_mod=last_mod, REQUEST=REQUEST):
-            return ''
-        feedtitle = self.folder().title_or_id() + ' new pages'
-        feeddescription = feedtitle
-        feedlanguage = 'en'
-        feeddate = self.folder().bobobase_modification_time().rfc822()
-        wikiurl = self.wikiUrl()
-        REQUEST.RESPONSE.setHeader('Content-Type','text/xml; charset=utf-8')
-        t = """\
-<rss version="2.0">
-<channel>
-<title>%(feedtitle)s</title>
-<link>%(feedurl)s</link>
-<description>%(feeddescription)s</description>
-<language>%(feedlanguage)s</language>
-<pubDate>%(feeddate)s</pubDate>
-""" % {
-            'feedtitle':self.toencoded(self.title_quote(feedtitle)),
-            'feeddescription':self.toencoded(html_quote(feeddescription)),
-            'feedurl':wikiurl,
-            'feedlanguage':feedlanguage,
-            'feeddate':feeddate,
-            }
-        for p in pages:
-            pobj = p.getObject()
-            t += """\
-<item>
-<title>%(title)s</title>
-<link>%(wikiurl)s/%(id)s</link>
-<guid>%(wikiurl)s/%(id)s</guid>
-<description><![CDATA[%(summary)s]]></description>
-<pubDate>%(creation_time)s</pubDate>
-</item>
-""" % {
-            'title':self.toencoded(self.title_quote(p.Title)),
-            'wikiurl':wikiurl,
-            'id':p.id,
-            'summary':self.toencoded(pobj.summary(1000)),
-            'creation_time':pobj.creationTime().rfc822(), # be robust here
-            }
-        #      <description><![CDATA[%(summary)s]]></description>
-        t += """\
-</channel>
-</rss>
-"""
-        return t
+        """Provide an RSS feed showing this wiki's recently created pages."""
+        return rssForPages(
+            self.pages(sort_on='creation_time',
+                       sort_order='reverse',
+                       sort_limit=num,
+                       isBoring=0),
+            ' new pages',
+            REQUEST=REQUEST)
 
     security.declareProtected(Permissions.View, 'edits_rss')
     def edits_rss(self, num=10, REQUEST=None):
@@ -88,17 +39,24 @@ class PageRSSSupport:
         pages. May be useful for monitoring, as a (less detailed)
         alternative to an all edits mail subscription.
         """
-        pages = self.pages(sort_on='last_edit_time',
-                            sort_order='reverse',
-                            sort_limit=num,
-                            isBoring=0)
+        return rssForPages(
+            self.pages(sort_on='last_edit_time',
+                       sort_order='reverse',
+                       sort_limit=num,
+                       isBoring=0),
+            ' changed pages',
+            REQUEST=REQUEST)
+
+    security.declareProtected(Permissions.View, 'rssForPages')
+    def rssForPages(self, pages, title_suffix='', REQUEST=None):
+        """Generate an RSS feed from the give page brains and title."""
         if len(pages) > 0:
             last_mod = pages[0].getObject().lastEditTime()
         else:
             last_mod = DateTime()
         if self.handle_modified_headers(last_mod=last_mod, REQUEST=REQUEST):
             return ''
-        feedtitle = self.folder().title_or_id() + ' changed pages'
+        feedtitle = self.folder().title_or_id() + title_suffix
         feeddescription = feedtitle
         feedlanguage = 'en'
         feeddate = self.folder().bobobase_modification_time().rfc822()
